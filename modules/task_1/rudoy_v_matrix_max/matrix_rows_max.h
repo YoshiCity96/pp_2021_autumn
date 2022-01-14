97c13ae216b5dc6fcf02c03b18dda70aff8185ec
YoshiCity96 <40843184+YoshiCity96@users.noreply.github.com>
2022-01-14 07:54:34 +0300

    Add files via upload

---
+++ modules/task_1/rudoy_v_matrix_max/matrix_rows_max.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Rudoy Vladislav
+#ifndef MODULES_TASK_1_RUDOY_V_MATRIX_MAX_MATRIX_ROWS_MAX_H_
+#define MODULES_TASK_1_RUDOY_V_MATRIX_MAX_MATRIX_ROWS_MAX_H_
+
+int* randomMatrix(int n, int m);
+int* sequentialMax(int* matrix, int n, int m);
+int* parallelMax(int* global_matrix, int n, int m);
+
+#endif  // MODULES_TASK_1_RUDOY_V_MATRIX_MAX_MATRIX_ROWS_MAX_H_
